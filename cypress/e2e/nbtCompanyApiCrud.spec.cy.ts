02981a46b4618038f420a22de2db86469363c08e
Sanwal Riaz <saleha.tahseen@kiwigrid.com>
2022-08-17 22:29:22 +0200

    change in api testing

--- cypress/e2e/nbtCompanyApiCrud.spec.cy.ts
+++ cypress/e2e/nbtCompanyApiCrud.spec.cy.ts
@@ -44,27 +44,15 @@ describe('NBT Company API Testing', () => {
   });
 
   it('Company API - GET', () => {
-    const UniqueNumber = `${Math.floor(Math.random() * 100)}`;
-    cy.request(`/company/${UniqueNumber}`)
-      .as('request')
-      .its('headers')
-      .its('content-type')
-      .should('include', 'application/json');
-    cy.get('@request').then((todos) => {
-      expect(todos.status).to.eq(200);
-    });
-  });
-
-  it('Company API - PUT', () => {
-    const UniqueNumber = `${Math.floor(Math.random() * 100)}`;
-    cy.request('PUT', `/company/${UniqueNumber}`, {
+    cy.request('POST', '/company', {
       name: 'Testing API Company',
       company_address: 'Address Testing Api Company',
       company_phone: '1111111111',
       company_email: 'testingcompany@gmail.com',
       company_website: 'www.testingapicrud.com',
-    }).then((response) => {
-      expect(response.status).to.eq(200);
+    }).then((response, res) => {
+      expect(response.status).to.eq(201);
+      cy.log(response.body);
       expect(response.body)
         .to.have.property('result')
         .to.have.property('name')
@@ -85,14 +73,115 @@ describe('NBT Company API Testing', () => {
         .to.have.property('result')
         .to.have.property('company_website')
         .to.eq('www.testingapicrud.com');
+      res = response.body.result.id;
+      cy.request(`/company/${res}`)
+        .as('request')
+        .its('headers')
+        .its('content-type')
+        .should('include', 'application/json');
+      cy.get('@request').then((todos) => {
+        expect(todos.status).to.eq(200);
+      });
     });
   });
 
+  it('Company API - PUT', () => {
+    cy.request('POST', '/company', {
+      name: 'Testing API Company',
+      company_address: 'Address Testing Api Company',
+      company_phone: '1111111111',
+      company_email: 'testingcompany@gmail.com',
+      company_website: 'www.testingapicrud.com',
+    }).then((response, res) => {
+      expect(response.status).to.eq(201);
+      cy.log(response.body);
+      expect(response.body)
+        .to.have.property('result')
+        .to.have.property('name')
+        .to.eq('Testing API Company');
+      expect(response.body)
+        .to.have.property('result')
+        .to.have.property('company_address')
+        .to.eq('Address Testing Api Company');
+      expect(response.body)
+        .to.have.property('result')
+        .to.have.property('company_phone')
+        .to.eq('1111111111');
+      expect(response.body)
+        .to.have.property('result')
+        .to.have.property('company_email')
+        .to.eq('testingcompany@gmail.com');
+      expect(response.body)
+        .to.have.property('result')
+        .to.have.property('company_website')
+        .to.eq('www.testingapicrud.com');
+      res = response.body.result.id;
+      cy.request('PUT', `/company/${res}`, {
+        name: 'Testing API Company',
+        company_address: 'Address Testing Api Company',
+        company_phone: '1111111111',
+        company_email: 'testingcompany@gmail.com',
+        company_website: 'www.testingapicrud.com',
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body)
+          .to.have.property('result')
+          .to.have.property('name')
+          .to.eq('Testing API Company');
+        expect(response.body)
+          .to.have.property('result')
+          .to.have.property('company_address')
+          .to.eq('Address Testing Api Company');
+        expect(response.body)
+          .to.have.property('result')
+          .to.have.property('company_phone')
+          .to.eq('1111111111');
+        expect(response.body)
+          .to.have.property('result')
+          .to.have.property('company_email')
+          .to.eq('testingcompany@gmail.com');
+        expect(response.body)
+          .to.have.property('result')
+          .to.have.property('company_website')
+          .to.eq('www.testingapicrud.com');
+      });
+    });
+  });
   it('Company API - DELETE', () => {
-    const UniqueNumber = `${Math.floor(Math.random() * 100)}`;
-    cy.request('DELETE', `/company/${UniqueNumber}`).then((response) => {
-      expect(response.status).to.eq(204);
-      expect(response.body).to.have.property('result').to.eq('true');
+    cy.request('POST', '/company', {
+      name: 'Testing API Company',
+      company_address: 'Address Testing Api Company',
+      company_phone: '1111111111',
+      company_email: 'testingcompany@gmail.com',
+      company_website: 'www.testingapicrud.com',
+    }).then((response, res) => {
+      expect(response.status).to.eq(201);
+      cy.log(response.body);
+      expect(response.body)
+        .to.have.property('result')
+        .to.have.property('name')
+        .to.eq('Testing API Company');
+      expect(response.body)
+        .to.have.property('result')
+        .to.have.property('company_address')
+        .to.eq('Address Testing Api Company');
+      expect(response.body)
+        .to.have.property('result')
+        .to.have.property('company_phone')
+        .to.eq('1111111111');
+      expect(response.body)
+        .to.have.property('result')
+        .to.have.property('company_email')
+        .to.eq('testingcompany@gmail.com');
+      expect(response.body)
+        .to.have.property('result')
+        .to.have.property('company_website')
+        .to.eq('www.testingapicrud.com');
+      res = response.body.result.id;
+      cy.request('DELETE', `/company/${res}`).then((response) => {
+        expect(response.status).to.eq(204);
+        expect(response.body).to.have.property('result').to.eq('true');
+      });
     });
   });
 });
